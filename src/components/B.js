75c029339b7a5f8786f532967d6c55ca88fff7ce
luvi-kh <nalsengom@daum.net>
2024-09-20 17:20:14 +0900

    useCallback 적용

--- src/components/B.js
+++ src/components/B.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 const Message = React.memo(({message}) => {
   return <p>{message}</p>
@@ -12,7 +12,8 @@ const ListItem = React.memo(({post}) => {
   )
 })
 
-const List = React.memo(({posts}) => {
+const List = React.memo(({posts, testFunction}) => {
+  console.log('List component is rendering')
   return (
     <ul>
       {posts.map(post => (
@@ -23,11 +24,13 @@ const List = React.memo(({posts}) => {
 })
 
 const B = ({message, posts}) => {
+  console.log('B component is rendering')
+  const testFunction= useCallback(() => {}, []);
   return (
     <div>
       <h1>B component</h1>
       <Message message={message}/>
-      <List posts={posts}/>
+      <List posts={posts} testFunction={testFunction}/>
     </div>
   )
 }
